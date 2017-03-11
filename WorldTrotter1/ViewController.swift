48afcb6cd5ef548d393cd2fa4739e89f2375176b
Rollin Francois <Rollin@Rollins-MacBook-Pro.local>
2017-03-11 18:02:00 -0500

    Remove frames / Add labels / Change background color

--- WorldTrotter1/ViewController.swift
+++ WorldTrotter1/ViewController.swift
@@ -13,15 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
         
-        let secondFrame = CGRect(x: 20, y: 20, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.gray
-        firstView.addSubview(secondView)
         
         
     }
