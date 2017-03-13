942c35f27544cb37d3aa9e3ff4316529982519ca
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-03-12 23:18:05 -0400

    Add new VC file

---
+++ WorldTrotter1/ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter1
+//
+//  Created by Rollin Francois on 3/12/17.
+//  Copyright © 2017 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
