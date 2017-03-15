c92109e1846ac7114e4feb46a629fa9905f4d82a
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-03-14 23:24:31 -0400

    Create an outlet to the Celsius test label and create an action for the text field to call when the text changes.

--- WorldTrotter1/ConversionViewController.swift
+++ WorldTrotter1/ConversionViewController.swift
@@ -10,4 +10,20 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     
+    // MARK: Properties
+    @IBOutlet var celciusLabel: UILabel!
+    
+    
+    
+    //MARK: Actions
+    @IBAction func farenheitFieldEditingChanged(_ textField: UITextField) {
+        
+        // celciusLabel.text = textField.text
+        if let text = textField.text, !text.isEmpty {
+            celciusLabel.text = text
+        } else {
+            celciusLabel.text = "???"
+        }
+    }
+    
 }
