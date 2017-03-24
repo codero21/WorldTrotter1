05eb003eb8c0221637c5644095dc2507edcb83f4
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-03-23 21:48:54 -0400

    Test Code - loading views

--- WorldTrotter1/ConversionViewController.swift
+++ WorldTrotter1/ConversionViewController.swift
@@ -45,6 +45,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("ConversionViewController loaded its view")
+        
         updateCelciusLabel()
     }
     
---
+++ WorldTrotter1/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  WorldTrotter1
+//
+//  Created by Rollin Francois on 3/19/17.
+//  Copyright © 2017 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view")
+    }
+    
+}
