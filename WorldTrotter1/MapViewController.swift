ae2f1406b8dfa6f245e03cb49984f6c34f95e88e
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-03-23 23:45:49 -0400

    Constrained segmented control

--- WorldTrotter1/MapViewController.swift
+++ WorldTrotter1/MapViewController.swift
@@ -7,9 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 
 class MapViewController: UIViewController {
+    
+    // MARK: Properties
+    var mapView: MKMapView!
+    
+    
+    
+    // MARK: Actions
+    override func loadView() {
+        // create map view
+        mapView = MKMapView()
+    
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        // create segmented control
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        // create constraints
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
